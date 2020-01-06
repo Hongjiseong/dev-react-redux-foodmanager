fe44719ed0f9a4182249de75969dc4ae0fc0de7b
Hong <dgf5820@naver.com>
2020-01-06 16:30:23 +0900

    Update profile image url

--- src/components/Header/Profile/profile.component.static.js
+++ src/components/Header/Profile/profile.component.static.js
@@ -4,7 +4,7 @@ const ProfileComponent = function (props) {
   return (
     <div className="profile">
       <div className="image">
-        <img src="../../../resources/images/Profile.png" alt="프로필 이미지" title="출처 [weibo.com/234604616]" />
+        <img src="https://i.pinimg.com/236x/eb/14/e1/eb14e14d2fcde310ea8de3fd97c0f8c8.jpg" alt="프로필 이미지" title="출처 [weibo.com/234604616]" />
       </div>
       <div className="summary">
         <p className="name">Hong Jiseong</p>
